e12ff7703599cb8ce10c51582fb06900e185f79e
Arslan Shakoor <arslantechielive@gmail.com>
2017-10-01 14:23:15 -0400

    got the new value

--- src/components/header.js
+++ src/components/header.js
@@ -21,7 +21,7 @@ export default class Header extends Component {
 
 getTitle(){
   return(
-    <span className="nav-email">{localStorage.email.toUpperCase()}</span>
+    <span className="nav-email">{localStorage.email}</span>
   );
 }
 
